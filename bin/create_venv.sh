cf5696a2a52d2d23620e8dfc4f5c4efe8a0281a0
Maksim Petrov <pmd@srv104073.hosttoname.com>
2025-03-11 23:57:33 +0100

    modifiy bin/create_venv.sh

--- bin/create_venv.sh
+++ bin/create_venv.sh
@@ -3,6 +3,9 @@
 prog_path=/usr/bot/pmd_food_diary_bot
 
 sudo apt install python3.10-venv
+
+rm -R $prog_path/venv
+
 python3 -m venv $prog_path/venv
 source $prog_path/venv/bin/activate
 pip install -r $prog_path/requirements.txt 
